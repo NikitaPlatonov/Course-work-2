f3a4c038c1edd080ddedbc48114b3d5751090c91
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-03 17:56:58 +0300

    fixed some points

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -11,7 +11,7 @@ public class Client {
     public static void main(String[] args) throws Exception {
         try(Socket socket = new Socket(host, port); PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-            out.println("[{" + "\"title\": " + "\"танки\", "+ "\"date\": " + "\"2022_02_08\", " + "\"sum\": " + "10000" + "}, {" + "\"title\": " + "\"молоко\", "+ "\"date\": " + "\"2022_02_09\", " + "\"sum\": " + "150" + "}]");
+            out.println("[{" + "\"title\": " + "\"Шахматный_курс_Максоуна\", "+ "\"date\": " + "\"2023_04_20\", " + "\"sum\": " + "3000" + "}]");
             out.flush();
             String serverMessage = in.readLine();
             String answer = in.readLine();
